d046fe2855f96fe3c5ad6476faa37f1cc70e69f7
Gene Romuga <generomuga12693@gmail.com>
2018-06-14 13:25:41 +0800

    Update get pixel values

--- prepare_training.py
+++ prepare_training.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_pixel_value(*args):
 	"""Get pixel value"""
@@ -6,8 +8,9 @@ def get_pixel_value(*args):
 	for x in xrange(args[0]):
 		for y in xrange(args[1]):
 			[value, alpha] = args[2][x, y]
-			list_pixel_value.append((x, y, value)) 
-			
+			#list_pixel_value.append((x, y, value)) 
+			list_pixel_value.append(value)
+
 	return list_pixel_value
 
 def get_image_size(image):
@@ -22,4 +25,12 @@ if __name__ == '__main__':
 	img = convert_to_grayscale('images\Ethel1.jpg')
 	im = img.load()
 	[width, height] = get_image_size(img)
-	get_pixel_value(width, height, im)
+	list_pixel_value = get_pixel_value(width, height, im)
+	
+
+
+	#plot values
+	#plt.plot(list_pixel_value)
+	#plt.ylabel("values")
+	#plt.xlabel("pixel no")
+	#plt.show()
