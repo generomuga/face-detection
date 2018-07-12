07cdedae6727e378870b680714a1d4f72a01d638
Gene Romuga <generomuga12693@gmail.com>
2018-07-12 14:58:19 +0800

    Added read pixel values to predict

--- predict.py
+++ predict.py
@@ -7,11 +7,32 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_decision_regions
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+
+def get_pixel_value(*args):
+	"""Get pixel value"""
+	list_pixel_value = []
+	for x in xrange(args[0]):
+		for y in xrange(args[1]):
+			[value, alpha] = args[2][x, y]
+			#list_pixel_value.append((x, y, value)) 
+			list_pixel_value.append(value)
+
+	return list_pixel_value
+
+def get_image_size(image):
+	"""Get image size"""
+	return image.size
+
+def convert_to_grayscale(image_name):
+	"""Convert image to grayscale"""
+	return Image.open(image_name).convert('LA')
 
 data = pd.read_csv("training_data\\train.csv").as_matrix()
 #clf = DecisionTreeClassifier()
-#clf = svm.SVC()
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2, 1), random_state=2, activation='relu')
+clf = svm.SVC()
+#clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 4, 3, 2, 1), random_state=2, activation='relu')
 
 #select from 1 to 21000 rows, from column 2 up to end column
 xtrain = data[0:8, 1:]
@@ -24,18 +45,21 @@ tran_label = data[0:8, 0]
 clf.fit(xtrain, tran_label)
 
 #testing data
-xtest = data[8:9:, 1:]
+"""xtest = data[8:9:, 1:]
 actual_label = data[8:9:, 0]
-print ('To predict',actual_label)
+print ('To predict',actual_label)"""
+
+img = convert_to_grayscale('to_predict\\input'+str(1)+'.jpg')
+im = img.load()
+[width, height] = get_image_size(img)
+		
+list_pixel_value = get_pixel_value(width, height, im)
+
 
 #d = xtest[0]
 #d.shape = (10,10)
 #plt.imshow(255-d, cmap='gray')
-print ('Result',clf.predict([xtest[0]]))
 
-"""Get the accuracy
-p = clf.predict(xtest)
-count = 0
-for i in range(0, 21000):
-	count +=1 if p[i] == actual_label[i] else 0
-print ("Accuracy ", (count/21000)*100)"""
+#print ('Result',clf.predict([xtest[0]]))
+print ('Result',clf.predict([list_pixel_value]))
+print (cross_val_score(clf, xtrain, tran_label, scoring='accuracy'))
