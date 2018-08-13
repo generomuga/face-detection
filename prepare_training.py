680ea94001674e0dda3991b461272fdc26a6d5fc
Gene Romuga <generomuga12693@gmail.com>
2018-08-13 16:56:16 +0800

    Updated reading of pixel values

--- prepare_training.py
+++ prepare_training.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 def get_pixel_value(*args):
 	"""Get pixel value"""
@@ -24,14 +25,18 @@ def convert_to_grayscale(image_name):
 def save_training_data(**kwargs):
 	file_train_data = open(kwargs['filename'], kwargs['method'])
 
+	"""Label"""
+	file_train_data.write(str(kwargs['label'])+',')
+	"""Pixel values"""
 	for i, item in enumerate(kwargs['data']):
 		file_train_data.write(str(item)+',')
 
 	file_train_data.write('\n')
 
 if __name__ == '__main__':
-	for i in range(1,6):
-		img = convert_to_grayscale('images\gene'+str(i)+'.jpg')
+	"""for i in range(1,3):
+		img = convert_to_grayscale('images\\DB of Faces_100x100_35 classes\\tan\\tan'+str(i)+'.png')
+		print (i)
 		im = img.load()
 		[width, height] = get_image_size(img)
 		
@@ -42,4 +47,16 @@ if __name__ == '__main__':
 		#plt.plot(list_pixel_value)
 		#plt.ylabel("values")
 		#plt.xlabel("pixel no")
-		#plt.show()
+		#plt.show()"""
+
+	for root, dirs, files in os.walk('E:\\Projects\\Python\\imageprocessing\\images\\DB of Faces_100x100 UPDATED (100 PERSON)'):
+		data = [(os.path.join(root,f)) for f in files]
+		for item in data:
+			img = convert_to_grayscale(item)
+			im = img.load()
+			[width, height] = get_image_size(img)
+			list_pixel_value = get_pixel_value(width, height, im)
+			dir, foldername = root.split(')\\')
+			save_training_data(filename='training_data\\train.csv', method='a', data=list_pixel_value, label=foldername)
+			#print (item, width, height)
+			
